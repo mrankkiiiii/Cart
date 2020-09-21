25f8bc21a06132d5dd2048d8b039335bd91e544e
Ankur Agarwal <agarg2311@gmail.com>
2020-09-21 13:25:21 +0530

    artist page implemented

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -1,14 +1,24 @@
 import React from 'react';
-
+import image from '../assets/images/personal.jpg';
 const Artists = (props) => {
     return(
         <div className="artists">
             <div className="topbar" >  
                     <p>i-Pod</p>
                     <img src="https://image.flaticon.com/icons/svg/3103/3103446.svg"></img>
-                </div>
+            </div>
             <div className="artists-container">
-            <p>Artists</p>
+                <div className="owner">
+                    <div className="owner-image">
+                        <img src={image} alt="owner"></img>
+                    </div>
+                    <div className="owner-info">
+                        <h4>IPod.Js <span><img src="https://www.flaticon.com/svg/static/icons/svg/953/953909.svg"></img></span></h4>
+                        <p>React Project</p>
+                        <p>Made By Ankur Agrawal</p>
+                    </div>
+                </div>
+                <p>Thanks for visiting 🙂</p>
             </div>
         </div>
     );
